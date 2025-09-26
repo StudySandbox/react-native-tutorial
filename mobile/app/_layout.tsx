35062890767d32e206754c0c0455ca60e00072d5
Insu Hong <hinsu93@gmail.com>
2025-09-26 11:31:53 +0900

    헤더 레이아웃 설정

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeScreen>
+      {/* 헤더 안보이도록 설정 */}
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeScreen>
+  );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -10,9 +10,6 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "purple",
   },
 });
